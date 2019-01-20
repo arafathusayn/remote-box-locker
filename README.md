8c9349f704dba4a2aa3df2d21df08a9548972ad8
Arafat Husayn <arafat@tuta.io>
2019-01-20 18:48:47 +0600

    commit first working release

--- README.md
+++ README.md
@@ -1,2 +1,84 @@
 # remote-box-locker
-Remotely lock/unlock your computer (Linux) with SSH Key + IP whitelisting
+
+Remotely lock/unlock your computer (Linux) using HTTP/JSON, SSH Key & IP whitelisting
+
+## System Requirements
+
+- node.js and npm
+- SSH Private Key (a password must be set)
+- `.env` file for setting up required configurations
+
+## Install
+
+```
+git clone https://github.com/arafathusayn/remote-box-locker.git && cd remote-box-locker && npm install
+```
+
+## Configuration
+
+Create a `.env` file in the `remote-box-locker` directory, then you can set these environment variables:
+
+```
+SERVER_PORT=3333
+
+USERNAME=<your computer username> 
+
+TRUST_PROXY=true or false
+
+IP_WHITELIST=<comma seperated IP addresses> eg. 192.168.0.100,192.168.0.101
+
+PRIVATE_KEY_PATH=/home/<USERNAME>/.ssh/id_rsa
+```
+
+### Notes
+
+- `USERNAME` env variable is **required**.
+- If your computer network is behind a trustable proxy, set `TRUST_PROXY=true` so that `IP_WHITELIST` can work properly. Default: `false`.
+- `IP_WHITELIST` always includes `localhost,127.0.0.1` as whitelisted
+- `PRIVATE_KEY_PATH` is your SSH private key path. It's optional. Default: `/home/<USERNAME>/.ssh/id_rsa`
+
+## Running server
+
+```
+node app.js
+```
+
+### If you use pm2:
+
+```
+pm2 start -f app.js
+```
+
+## API Usage
+
+### Locking
+
+To **lock** the computer running the server, send HTTP `POST` request to <br>
+```
+Endpoint: /lock
+
+eg. http://localhost:3333/lock
+```
+along with HTTP Header `Content-Type:` `application/json` and the JSON data in POST Body:
+
+```
+{
+  "password": "<Your SSH Private Key Password>"
+}
+```
+
+### Unlocking
+
+To **unlock** the computer running the server, send HTTP `POST` request to <br>
+```
+Endpoint: /unlock
+
+eg. http://localhost:3333/unlock
+```
+along with HTTP Header `Content-Type:` `application/json` and the JSON data in POST Body:
+
+```
+{
+  "password": "<Your SSH Private Key Password>"
+}
+```
--- app.js
+++ app.js
@@ -1,48 +1,153 @@
 const fs = require("fs");
 const path = require("path");
+const crypto = require("crypto");
+const { exec } = require("child_process");
 const express = require("express");
 const app = express();
 const helmet = require("helmet");
 const bodyParser = require("body-parser");
-const SSHClient = require("ssh2").Client;
-const whilelist = require("./ip-whitelist.json");
 
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const envFileExists = fs.existsSync(`${__dirname}/.env`);
+const envFileExists = fs.existsSync(path.resolve(__dirname, ".env"));
 
 if (envFileExists) {
   require("dotenv").config();
 }
 
 const SERVER_PORT = process.env.SERVER_PORT || 3333;
-const host = process.env.HOST || "localhost";
-const port = process.env.PORT || 22;
 const username = process.env.USERNAME || "root";
 const privateKeyPath = process.env.PRIVATE_KEY_PATH || `/home/${username}/.ssh/id_rsa`;
-const privateKey = fs.readFileSync(privateKeyPath);
+const trustProxy = process.env.TRUST_PROXY == "true" ? true : false;
+const whilelist = process.env.IP_WHITELIST.split(',').concat(['localhost', '127.0.0.1']) || ['localhost', '127.0.0.1'];
+
+if (trustProxy) {
+  app.set("trust proxy", trustProxy);
+}
+
+function getRemoteAddress(address) {
+  address = address.replace("::ffff:", "");
+
+  if (address == "::1") {
+    return "localhost";
+  }
+
+  return address;
+}
 
 app.get("/", (req, res) => {
-    res.json({
-        remoteAddress: req.ip || req.connection.remoteAddress,
+  const remoteAddress = getRemoteAddress(req.ip);
+
+  res.json({
+    remoteAddress,
+  });
+});
+
+app.post("/lock", (req, res) => {
+  const clientIPAddress = getRemoteAddress(req.ip);
+
+  console.log(`\n[${new Date()}] ${clientIPAddress} is trying to lock\n`);
+
+  if (!req.body.password) {
+    return res.status(401).json({
+      success: false,
+      error: "Invalid password",
     });
+  }
+
+  const key = fs.readFileSync(privateKeyPath, "utf8");
+  const passphrase = req.body.password;
+
+  let result = {
+    success: false,
+  };
+
+  try {
+    crypto
+      .privateEncrypt(
+        {
+          key,
+          passphrase,
+        },
+        Buffer.from("")
+      );
+
+    result.success = true;
+  } catch (err) {
+    if (err.opensslErrorStack) {
+      result.error = "Wrong password";
+    }
+  }
+
+  if (result.success && !result.error & whilelist.includes(clientIPAddress)) {
+    exec("loginctl lock-session");
+    result.client = clientIPAddress;
+  } else if (!whilelist.includes(clientIPAddress)) {
+    result.success = false;
+    result.error = "Client IP Address is not whitelisted";
+  } else {
+    result.success = false;
+  }
+
+  console.log(`\n[${new Date()}] ${clientIPAddress} locked and received: ${JSON.stringify(result, null, 2)}\n`);
+
+  res.json(result);
 });
 
-app.post("/lock", () => {
+app.post("/unlock", (req, res) => {
+  const clientIPAddress = getRemoteAddress(req.ip);
 
-    // const conn = new SSHClient();
-    // conn.connect({
-    //     host,
-    //     port,
-    //     username,
-    //     privateKey,
-    // });
+  console.log(`\n[${new Date()}] ${clientIPAddress} is trying to unlock\n`);
+
+  if (!req.body.password) {
+    return res.status(401).json({
+      success: false,
+      error: "Invalid password",
+    });
+  }
+
+  const key = fs.readFileSync(privateKeyPath, "utf8");
+  const passphrase = req.body.password;
+
+  let result = {
+    success: false,
+  };
+
+  try {
+    crypto
+      .privateEncrypt(
+        {
+          key,
+          passphrase,
+        },
+        Buffer.from("")
+      );
+
+    result.success = true;
+  } catch (err) {
+    if (err.opensslErrorStack) {
+      result.error = "Wrong password";
+    }
+  }
+
+  if (result.success && !result.error && whilelist.includes(clientIPAddress)) {
+    exec(`loginctl unlock-session`);
+
+    result.client = clientIPAddress;
+  } else if (!whilelist.includes(clientIPAddress)) {
+    result.success = false;
+    result.error = "Client IP Address is not whitelisted";
+  } else {
+    result.success = false;
+  }
+
+  console.log(`\n[${new Date()}] ${clientIPAddress} unlocked and received: ${JSON.stringify(result, null, 2)}\n`);
+
+  res.json(result);
 });
-
-app.post("/unlock", () => {});
 
 app.listen(SERVER_PORT, () => {
-    console.log(`> Listening http://localhost:${SERVER_PORT}`);
+  console.log(`> Listening http://localhost:${SERVER_PORT}`);
 });
